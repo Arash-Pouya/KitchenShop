0c9738b602954c267a569e57c7719591babf78af
arash-pouya <arash.pouya.c@gmail.com>
2021-11-05 15:34:12 +0330

    Home page slider compelete

--- Script/javascript.js
+++ Script/javascript.js
@@ -96,3 +96,13 @@ $(document).ready(function () {
   });
 });
 // work in progress - needs some refactoring and will drop JQuery i promise :)
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
